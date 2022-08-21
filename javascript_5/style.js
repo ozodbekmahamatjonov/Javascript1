538b6405547cd1efc9e20c603dcf1304a27006a4
ozodbek <111683449+ozodbekmahamatjonov@users.noreply.github.com>
2022-08-21 15:14:48 +0500

    Add files via upload

---
+++ javascript_5/style.js
@@ -0,0 +1,9 @@
+let name = prompt('Ismingizni kiritng');
+let takror = prompt('Ismingizni necha marta takrorlay?');
+for(let i = 0; i < 6; i++) {
+    alert(`${i} ozodbek`);
+}
+let takror1 = prompt('Ismingizni necha marta takrorlay?');
+for(let i = 0; i < 11; i++) {
+    alert(`${i} ozodbek`);
+}
